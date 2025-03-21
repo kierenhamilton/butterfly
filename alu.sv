e8086969673a3387ebe3e3a48826b31ad49266cd
kieren hamilton <kierenhamilton1@outlook.com>
2025-03-21 22:01:23 +0000

    wokring butterfly

--- alu.sv
+++ alu.sv
@@ -1,14 +1,16 @@
 module alu (
-  input Clock, 
-  input nReset,
-  input signed [7:0] a, 
-  input signed [15:0] b,
-  output signed logic o
+  input [1:0] opperation,
+  input signed [7:0] alu_input_1,
+  input signed [7:0] alu_input_2,
+  output logic signed [15:0] alu_output
   );
 
-  timeunit 1ns; 
-  timeprecision 100ps; 
-
-assign o = a * b; 
-
+  always_comb
+    case (opperation)
+      0: alu_output = alu_input_1 * alu_input_2;
+      1: alu_output = alu_input_1 + alu_input_2; 
+      2: alu_output = alu_input_1 - alu_input_2; 
+    endcase
   endmodule
+
+  
--- butterfly_stim.sv
+++
@@ -1,6 +0,0 @@
-module butterfly_stim; 
-
-timeunit 1ns; 
-timeunit 100ps; 
-
-endmodule
